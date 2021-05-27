0816bead234db974bc68396b98b2123bfe29890e
denefc <mehmet_7kezman@hotmail.com>
2021-05-27 13:19:35 +0300

    dynmic content added w/ejs

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const ejs = require("ejs");
 const path = require("path");
+const Photo=require('./models/Photo');
+
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db',{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+});
+
 
 //templates engine
 app.set("view engine", "ejs");
 
 //mıddleware
 app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.render("index");
+//Routes
+app.get("/", async(req, res) => {
+  const photos=await Photo.find({})
+  res.render("index",{
+    photos
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
 });
 app.get("/add", (req, res) => {
   res.render("add");
+});
+
+app.post("/photos", (req, res) => {
+  Photo.create(req.body)
+  res.redirect("/")
 });
 
 const port = 3000;
