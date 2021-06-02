63acd1dc15180d6f9cda6d37ad5c82cdaaca11c0
denefc <mehmet_7kezman@hotmail.com>
2021-06-02 22:11:37 +0300

    update photos added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const ejs = require("ejs");
 const path = require("path");
 const Photo=require('./models/Photo');
+const methodOverride = require('method-override');
 
 
 const app = express();
@@ -26,6 +27,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(fileUpload())
+app.use(methodOverride('_method'));
 
 //Routes
 app.get("/", async(req, res) => {
@@ -66,6 +68,22 @@ uploadeImage.mv(uploadPath, async () => {
   });
   res.redirect('/');
 });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+
+  res.redirect(`/photos/${req.params.id}`)
 });
 
 const port = 3000;
