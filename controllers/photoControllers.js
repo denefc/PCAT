a9ce9e58862989ea912853ec14b3da1c18b1bce5
denefc <mehmet_7kezman@hotmail.com>
2021-06-04 20:24:23 +0300

    finished the project

--- controllers/photoControllers.js
+++ controllers/photoControllers.js
@@ -1,36 +1,54 @@
-const Photo=require('../models/Photo')
+const Photo = require('../models/Photo');
+const fs = require('fs');
 
 exports.getAllPhotos = async (req, res) => {
-  const photos = await Photo.find({}).sort("-dateCreated");
-  res.render("index", {
-    photos,
+
+  const page = req.query.page || 1;
+  const photosPerPage = 3;// kaç foto göstermek istiyosak
+
+  const totalPhotos = await Photo.find().countDocuments();
+
+  const photos = await Photo.find({})
+  .sort('-dateCreated')
+  .skip((page-1) * photosPerPage)
+  .limit(photosPerPage)
+
+  res.render('index', {
+    photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage)
   });
+
+/*   console.log(req.query);
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
+  }); */
 };
 
 exports.getPhoto = async (req, res) => {
-  const id = req.params.id;
-  const photo = await Photo.findById(id);
-  res.render("photo", {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
     photo,
   });
 };
 
-exports.createPhoto = (req, res) => {
-  const uploadDir = "public/uploads";
+exports.createPhoto = async (req, res) => {
+  const uploadDir = 'public/uploads';
 
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
   }
 
   let uploadeImage = req.files.image;
-  let uploadPath = __dirname + "/public/uploads/" + uploadeImage.name;
+  let uploadPath = __dirname + '/../public/uploads/' + uploadeImage.name;
 
   uploadeImage.mv(uploadPath, async () => {
     await Photo.create({
       ...req.body,
-      image: "/uploads/" + uploadeImage.name,
+      image: '/uploads/' + uploadeImage.name,
     });
-    res.redirect("/");
+    res.redirect('/');
   });
 };
 
@@ -45,8 +63,8 @@ exports.updatePhoto = async (req, res) => {
 
 exports.deletePhoto = async (req, res) => {
   const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname + "/public" + photo.image;
+  let deletedImage = __dirname + '/../public' + photo.image;
   fs.unlinkSync(deletedImage);
   await Photo.findByIdAndRemove(req.params.id);
-  res.redirect("/");
+  res.redirect('/');
 };
